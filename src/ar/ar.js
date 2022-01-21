2f1ed238d29562c45e8d5ce6ad6d1486bb316e5d
masashi_takizawa <taki.digi@gmail.com>
2022-01-21 10:14:24 +0900

    sourceWidth + sourceHeight add.

--- src/ar/ar.js
+++ src/ar/ar.js
@@ -1,108 +1,92 @@
-import { useFrame, useThree } from "@react-three/fiber"
-import { ArToolkitContext, ArToolkitSource } from "@ar-js-org/ar.js/three.js/build/ar-threex"
-import React, { createContext, useCallback, useEffect, useMemo } from "react"
+import { useFrame, useThree } from '@react-three/fiber';
+import { ArToolkitContext, ArToolkitSource } from '@ar-js-org/ar.js/three.js/build/ar-threex';
+import React, { createContext, useCallback, useEffect, useMemo } from 'react';
 
-const ARContext = createContext({})
-const videoDomElemSelector = "#arjs-video"
+const ARContext = createContext({});
+const videoDomElemSelector = '#arjs-video';
 
-const AR = React.memo(({
-  tracking = true,
-  children,
-  sourceType,
-  patternRatio,
-  matrixCodeType,
-  detectionMode,
-  cameraParametersUrl,
-  onCameraStreamReady,
-  onCameraStreamError
-}) => {
-  const { gl, camera } = useThree()
+const AR = React.memo(({ tracking = true, children, sourceType, patternRatio, matrixCodeType, detectionMode, sourceWidth, sourceHeight, cameraParametersUrl, onCameraStreamReady, onCameraStreamError }) => {
+  const { gl, camera } = useThree();
 
   const arContext = useMemo(() => {
-    const arToolkitSource = new ArToolkitSource({ sourceType })
+    const arToolkitSource = new ArToolkitSource({ sourceWidth, sourceHeight, sourceType });
     const arToolkitContext = new ArToolkitContext({
       cameraParametersUrl,
       detectionMode,
       patternRatio,
-      matrixCodeType
-    })
+      matrixCodeType,
+    });
 
-    return { arToolkitContext, arToolkitSource }
-  }, [patternRatio, matrixCodeType, cameraParametersUrl, detectionMode, sourceType])
+    return { arToolkitContext, arToolkitSource };
+  }, [patternRatio, matrixCodeType, cameraParametersUrl, detectionMode, sourceWidth, sourceHeight, sourceType]);
 
   const onResize = useCallback(() => {
-    const { arToolkitContext, arToolkitSource } = arContext
+    const { arToolkitContext, arToolkitSource } = arContext;
 
-    arToolkitSource.onResizeElement()
-    arToolkitSource.copyElementSizeTo(gl.domElement)
+    arToolkitSource.onResizeElement();
+    arToolkitSource.copyElementSizeTo(gl.domElement);
     if (arToolkitContext.arController !== null) {
-      arToolkitSource.copyElementSizeTo(arToolkitContext.arController.canvas)
-      camera.projectionMatrix.copy(arToolkitContext.getProjectionMatrix())
+      arToolkitSource.copyElementSizeTo(arToolkitContext.arController.canvas);
+      camera.projectionMatrix.copy(arToolkitContext.getProjectionMatrix());
     }
-  }, [gl, arContext, camera])
+  }, [gl, arContext, camera]);
 
   const onUnmount = useCallback(() => {
-    window.removeEventListener("resize", onResize)
+    window.removeEventListener('resize', onResize);
 
-    arContext.arToolkitContext.arController.dispose()
+    arContext.arToolkitContext.arController.dispose();
     if (arContext.arToolkitContext.arController.cameraParam) {
-      arContext.arToolkitContext.arController.cameraParam.dispose()
+      arContext.arToolkitContext.arController.cameraParam.dispose();
     }
 
-    delete arContext.arToolkitContext
-    delete arContext.arToolkitSource
+    delete arContext.arToolkitContext;
+    delete arContext.arToolkitSource;
 
-    const video = document.querySelector(videoDomElemSelector)
+    const video = document.querySelector(videoDomElemSelector);
     if (video) {
-      video.srcObject.getTracks().map(track => track.stop())
-      video.remove()
+      video.srcObject.getTracks().map((track) => track.stop());
+      video.remove();
     }
-  }, [onResize, arContext])
+  }, [onResize, arContext]);
 
   useEffect(() => {
     arContext.arToolkitSource.init(() => {
-      const video = document.querySelector(videoDomElemSelector)
-      video.style.position = "fixed"
+      const video = document.querySelector(videoDomElemSelector);
+      video.style.position = 'fixed';
 
       video.onloadedmetadata = () => {
         if (onCameraStreamReady) {
-          onCameraStreamReady()
+          onCameraStreamReady();
         }
-        onResize()
-      }
-    }, onCameraStreamError)
+        onResize();
+      };
+    }, onCameraStreamError);
 
-    arContext.arToolkitContext.init(() =>
-      camera.projectionMatrix.copy(arContext.arToolkitContext.getProjectionMatrix())
-    )
+    arContext.arToolkitContext.init(() => camera.projectionMatrix.copy(arContext.arToolkitContext.getProjectionMatrix()));
 
-    window.addEventListener("resize", onResize)
+    window.addEventListener('resize', onResize);
 
-    return onUnmount
-  }, [arContext, camera, onCameraStreamReady, onCameraStreamError, onResize, onUnmount])
+    return onUnmount;
+  }, [arContext, camera, onCameraStreamReady, onCameraStreamError, onResize, onUnmount]);
 
   useFrame(() => {
     if (!tracking) {
-      return
+      return;
     }
 
     if (arContext.arToolkitSource && arContext.arToolkitSource.ready !== false) {
-      arContext.arToolkitContext.update(arContext.arToolkitSource.domElement)
+      arContext.arToolkitContext.update(arContext.arToolkitSource.domElement);
     }
-  })
+  });
 
-  const value = useMemo(() => ({ arToolkitContext: arContext.arToolkitContext }), [arContext])
+  const value = useMemo(() => ({ arToolkitContext: arContext.arToolkitContext }), [arContext]);
 
-  return (
-    <ARContext.Provider value={ value }>
-      { children }
-    </ARContext.Provider>
-  )
-})
+  return <ARContext.Provider value={value}>{children}</ARContext.Provider>;
+});
 
 const useAR = () => {
-  const arValue = React.useContext(ARContext)
-  return React.useMemo(() => ({ ...arValue }), [arValue])
-}
+  const arValue = React.useContext(ARContext);
+  return React.useMemo(() => ({ ...arValue }), [arValue]);
+};
 
-export { AR, useAR }
+export { AR, useAR };
--- src/ar/arCanvas.js
+++ src/ar/arCanvas.js
@@ -2,39 +2,21 @@
 /* eslint-disable react/jsx-indent */
 /* eslint-disable react/jsx-pascal-case */
 
-import React from "react"
-import { Canvas } from "@react-three/fiber"
+import React from 'react';
+import { Canvas } from '@react-three/fiber';
 
-import { AR } from "./ar"
+import { AR } from './ar';
 
-const ARCanvas = ({
-  arEnabled = true,
-  tracking = true,
-  children,
-  patternRatio = 0.5,
-  detectionMode = "mono_and_matrix",
-  cameraParametersUrl = "data/camera_para.dat",
-  matrixCodeType = "3x3",
-  sourceType = "webcam",
-  onCameraStreamReady,
-  onCameraStreamError,
-  ...props }) =>
-  <Canvas camera={ arEnabled ? { position: [0, 0, 0] } : props.camera } { ...props }>
-    {
-      arEnabled
-        ? <AR
-            tracking={ tracking }
-            patternRatio={ patternRatio }
-            matrixCodeType={ matrixCodeType }
-            detectionMode={ detectionMode }
-            sourceType={ sourceType }
-            cameraParametersUrl={ cameraParametersUrl }
-            onCameraStreamReady={ onCameraStreamReady }
-            onCameraStreamError={ onCameraStreamError }>
-              { children }
-          </AR>
-        : children
-    }
+const ARCanvas = ({ arEnabled = true, tracking = true, children, patternRatio = 0.5, detectionMode = 'mono_and_matrix', cameraParametersUrl = 'data/camera_para.dat', matrixCodeType = '3x3', sourceType = 'webcam', onCameraStreamReady, onCameraStreamError, sourceWidth = 640, sourceHeight = 480, ...props }) => (
+  <Canvas camera={arEnabled ? { position: [0, 0, 0] } : props.camera} {...props}>
+    {arEnabled ? (
+      <AR tracking={tracking} patternRatio={patternRatio} matrixCodeType={matrixCodeType} detectionMode={detectionMode} sourceType={sourceType} sourceWidth={sourceWidth} sourceHeight={sourceHeight} cameraParametersUrl={cameraParametersUrl} onCameraStreamReady={onCameraStreamReady} onCameraStreamError={onCameraStreamError}>
+        {children}
+      </AR>
+    ) : (
+      children
+    )}
   </Canvas>
+);
 
-export default ARCanvas
+export default ARCanvas;
